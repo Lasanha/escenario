36afc469434a20792080307a4d410122d10f4147
Gabriel Marcondes <gabrielgeraldo@gmail.com>
2013-11-04 17:50:26 -0200

    fixing links to recent

--- generator/models.py
+++ generator/models.py
@@ -15,7 +15,7 @@ class Esc(models.Model):
 class EscImg(models.Model):
     criado_em = models.DateTimeField(auto_now_add=True)
     esc = models.ForeignKey('Esc')
-    img_id = models.CharField(max_length=50, default=lambda: str(datetime.datetime.now()) + 'jpg')
+    img_id = models.CharField(max_length=50, default=lambda: str(hash(datetime.datetime.now())) + 'jpg')
 
 
     def draw(self):
