f59c6e30ae5f566526d756cd4e3e4ac8e43b2c3a
Gabriel Marcondes <gabrielgeraldo@gmail.com>
2013-11-04 15:28:30 -0200

    including south into installed apps

--- escenario/settings.py
+++ escenario/settings.py
@@ -128,6 +128,7 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
     'generator',
+    'south',
 )
 
 SESSION_SERIALIZER = 'django.contrib.sessions.serializers.JSONSerializer'
