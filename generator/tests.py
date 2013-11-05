ee1c6bcb074755b0290856c56049d9fd3881dc7f
Gabriel Marcondes <gabrielgeraldo@gmail.com>
2013-11-05 18:33:49 -0200

    tests for splitting tasks from draw

--- generator/tests.py
+++ generator/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from model_mommy import mommy
 from models import Esc, EscImg
+import os
 
 class EscTest(TestCase):
     def test_esc_creation(self):
@@ -20,3 +21,34 @@ class EscTest(TestCase):
         self.assertTrue(isinstance(esc_img, EscImg))
         self.assertEqual(esc_img.__unicode__(), esc_img.esc.titulo)
 
+
+    def test_esc_img_prepare(self):
+        """
+        Tests img preparation (copy)
+        """
+        esc_img = mommy.make(EscImg)
+        alvo = esc_img.prepare()
+        self.assertTrue(os.path.isfile(alvo))
+
+
+    def test_esc_img_draw(self):
+        """
+        Tests img draw
+        """
+        esc_img = mommy.make(EscImg)
+        alvo = esc_img.prepare()
+        esc_img.draw(alvo)
+        self.assertTrue(os.path.isfile(alvo))
+
+
+    def test_esc_img_upload(self):
+        """
+        Tests img upload
+        """
+        esc_img = mommy.make(EscImg)
+        alvo = esc_img.prepare()
+        esc_img.draw(alvo)
+        esc_img.upload(alvo)
+        self.assertTrue('i.imgur.com' in esc_img.img_id)
+        
+        
