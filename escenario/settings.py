313e024fb39c3060a804c175687506e91b0dfc49
Gabriel Marcondes <gabrielgeraldo@gmail.com>
2013-11-05 13:34:41 -0200

    configuring static for debug=false

--- escenario/settings.py
+++ escenario/settings.py
@@ -3,7 +3,7 @@ import os
 import sys
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-DEBUG = False
+DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
@@ -177,7 +177,7 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 ALLOWED_HOSTS = ['*']
 
-STATIC_ROOT = 'static'
+STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
--- escenario/urls.py
+++ escenario/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.conf import settings
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -18,4 +19,11 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
-)
+
+    )
+
+if settings.DEBUG is False:
+    urlpatterns += patterns('', 
+        url(r'^static/(?P<path>.*)$', 'django.views.static.serve', 
+            {'document_root': settings.STATIC_ROOT}),
+        )
