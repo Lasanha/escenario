0540c157ddbfd9898c4036751bd9eca9cabb76d1
Gabriel Marcondes <gabrielgeraldo@gmail.com>
2013-11-06 18:45:00 -0200

    forcing uppercase on title, changing text config to environs

--- generator/models.py
+++ generator/models.py
@@ -5,8 +5,8 @@ import os, escenario.settings
 from PIL import Image, ImageFont, ImageDraw
 import textwrap
 import pyimgur
-IMGUR_API = os.environ.get('IMGUR_API', None)
 
+IMGUR_API = os.environ.get('IMGUR_API', None)
 
 class Esc(models.Model):
     titulo = models.CharField(max_length=30)
@@ -37,17 +37,21 @@ class EscImg(models.Model):
 
 
     def draw(self, alvo):
+        F_TITULO = int(os.environ.get('F_TITULO', 34))
+        F_FALTAM = int(os.environ.get('F_FALTAM', 34))
+        F_TEXTO = int(os.environ.get('F_TEXTO', 12))
+        F_WRAP = int(os.environ.get('F_WRAP', 40))
         img = Image.open(alvo)
         font_title = os.path.join(escenario.settings.BASE_DIR, 'ROADWAY_.TTF')
         font_text = os.path.join(escenario.settings.BASE_DIR, 'kharon.ttf')
-        font_titulo = ImageFont.truetype(font_title, 34)
-        font_faltam = ImageFont.truetype(font_title, 34)
-        font_descricao = ImageFont.truetype(font_text, 12)
-        linhas = textwrap.wrap(self.esc.descricao, width=44)
+        font_titulo = ImageFont.truetype(font_title, F_TITULO)
+        font_faltam = ImageFont.truetype(font_title, F_FALTAM)
+        font_descricao = ImageFont.truetype(font_text, F_TEXTO)
+        linhas = textwrap.wrap(self.esc.descricao, width=F_WRAP)
         y_text = 90
         draw = ImageDraw.Draw(img)
-        draw.text((20,10), self.esc.titulo, (255,255,255), font=font_titulo)
-        draw.text((80,47), self.esc.faltam, (150,255,0), font=font_faltam)
+        draw.text((20,10), self.esc.titulo.upper(), (255,255,255), font=font_titulo)
+        draw.text((80,47), self.esc.faltam.upper(), (150,255,0), font=font_faltam)
         for linha in linhas:
             w, h = font_descricao.getsize(linha)
             draw.text((15, y_text), linha, (255,255,255), font=font_descricao)
