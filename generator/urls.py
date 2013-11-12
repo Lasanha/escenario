56b711613046da23f87497cb172fd13aa176325e
Gabriel Marcondes <gabrielgeraldo@gmail.com>
2013-11-12 14:21:48 -0200

    initial vote engine

--- generator/urls.py
+++ generator/urls.py
@@ -1,5 +1,9 @@
 from django.conf.urls import patterns, include, url
 from views import Home, About, List, Restricted
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from dajaxice.core import dajaxice_autodiscover, dajaxice_config
+
+dajaxice_autodiscover()
 
 urlpatterns = patterns('',
     url(r'^$', Home.as_view()),
@@ -9,4 +13,8 @@ urlpatterns = patterns('',
     url(r'^restricted/$', Restricted.as_view()),
     url(r'^api/list/$', 'generator.views.api_list'),
     url(r'^api/create/$', 'generator.views.api_create'),
+
+    url(dajaxice_config.dajaxice_url, include('dajaxice.urls')),
 )
+
+urlpatterns += staticfiles_urlpatterns()
