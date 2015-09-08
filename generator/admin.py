f23cea34489947dce985169c739e440245985941
Gabriel Marcondes <gabrielgeraldo@gmail.com>
2015-09-08 00:51:39 -0300

    read only fields on admin

--- generator/admin.py
+++ generator/admin.py
@@ -4,6 +4,12 @@ from generator.models import Esc, EscImg
 
 class EscImgAdmin(admin.ModelAdmin):
     raw_id_fields = ['esc']
+    readonly_fields = ['criado_em']
+
+
+class EscAdmin(admin.ModelAdmin):
+    readonly_fields = ['criado_em']
+
 
 admin.site.register(EscImg, EscImgAdmin)
-admin.site.register(Esc)
+admin.site.register(Esc, EscAdmin)
