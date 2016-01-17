e2fce8a2206e90a4d67ff030d7cbfd9bfdce5056
Gabriel Marcondes <gabrielgeraldo@gmail.com>
2016-01-17 14:58:30 -0200

    changing urlpatterns to a list of urls (removing deprecated way)

--- escenario/urls.py
+++ escenario/urls.py
@@ -1,29 +1,18 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 from django.conf import settings
-
-# Uncomment the next two lines to enable the admin:
 from django.contrib import admin
+from django.contrib.auth.views import login, logout_then_login
+from django.views.static import serve as static_serve
+from django_summernote import urls as summernote_urls
+
 from generator.urls import urlpatterns as gen_urls
 
 admin.autodiscover()
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'escenario.views.home', name='home'),
-    # url(r'^escenario/', include('escenario.foo.urls')),
-
+urlpatterns = [
     url(r'', include(gen_urls)),
-    url(r'^summernote/', include('django_summernote.urls')),
-    url(r'^static/(?P<path>.*)$', 'django.views.static.serve', 
-        {'document_root': settings.STATIC_ROOT}),
-    url(r'login/$', 'django.contrib.auth.views.login', {'template_name': 'login.html'}),
-    url(r'logout/$', 'django.contrib.auth.views.logout_then_login', {}),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
-
-    )
-
+    url(r'^summernote/', include(summernote_urls)),
+    url(r'^static/(?P<path>.*)$', static_serve, {'document_root': settings.STATIC_ROOT}),
+    url(r'login/$', login, {'template_name': 'login.html'}),
+    url(r'logout/$', logout_then_login, {}),
+]
--- generator/urls.py
+++ generator/urls.py
@@ -1,9 +1,9 @@
-from django.conf.urls import url, patterns, include
+from django.conf.urls import url, include
 from generator.views import Home, About, List, Restricted, NewMicroblogPost
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.contrib import admin
 
-urlpatterns = patterns('',
+urlpatterns = [
     url(r'^$', Home.as_view(), name='home'),
     url(r'^view/(?P<esc_id>\d+)/$', Home.as_view(), name='view'),
     url(r'^list/$', List.as_view(criterio='-criado_em'), name='list'),
@@ -17,7 +17,7 @@ urlpatterns = patterns('',
     #url(r'^api/list/$', 'generator.views.api_list', name='api_list'),
     #url(r'^api/create/$', 'generator.views.api_create', name='api_create'),
     #url(r'^api/vote/(?P<escimg_id>\d+)$', 'generator.views.api_vote', name='api_vote'),
-)
+]
 
 urlpatterns += staticfiles_urlpatterns()
 
