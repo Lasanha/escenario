a54f0a40158b29f29bed0e5d49478b5e32ddc3f1
Gabriel Marcondes <gabrielgeraldo@gmail.com>
2017-01-20 16:36:44 -0200

    cleanup manage.py

--- escenario/settings.py
+++ escenario/settings.py
@@ -1,8 +1,7 @@
-# Django settings for escenario project.
 import os
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-DEBUG = os.environ.get('ESCENARIO_DEBUG', False)
+DEBUG = os.environ.get('ESCENARIO_DEBUG', 'false') == 'true'
 
 if DEBUG:
     GA_CODE = ''
@@ -14,95 +13,44 @@ else:
     GADSENSE_SLOT = os.environ.get('GADSENSE_SLOT', '')
 
 ADMINS = (
-    # ('Your Name', 'your_email@example.com'),
+    ('Gabriel Marcondes', os.environ.get('ADMIN_EMAIL', 'test@example.com')),
 )
 
 MANAGERS = ADMINS
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.',
-        'NAME': '',                      # Or path to database file if using sqlite3.
-        # The following settings are not used with sqlite3:
-        'USER': '',
-        'PASSWORD': '',
-        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'PORT': '',                      # Set to empty string for default.
-    }
-}
-
-
-# Hosts/domain names that are valid for this site; required if DEBUG is False
-# See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = ['*']
 
-# Local time zone for this installation. Choices can be found here:
-# http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
-# although not all choices may be available on all operating systems.
-# In a Windows environment this must be set to your system time zone.
 TIME_ZONE = 'America/Chicago'
 
-# Language code for this installation. All choices can be found here:
-# http://www.i18nguy.com/unicode/language-identifiers.html
 LANGUAGE_CODE = 'en-us'
 
 SITE_ID = 1
 
-# If you set this to False, Django will make some optimizations so as not
-# to load the internationalization machinery.
-USE_I18N = True
-
-# If you set this to False, Django will not format dates, numbers and
-# calendars according to the current locale.
-USE_L10N = True
-
-# If you set this to False, Django will not use timezone-aware datetimes.
+USE_I18N = False
+USE_L10N = False
 USE_TZ = True
 
-# Absolute filesystem path to the directory that will hold user-uploaded files.
-# Example: "/var/www/example.com/media/"
 MEDIA_ROOT = ''
-
-# URL that handles the media served from MEDIA_ROOT. Make sure to use a
-# trailing slash.
-# Examples: "http://example.com/media/", "http://media.example.com/"
 MEDIA_URL = ''
-
-# Absolute path to the directory static files should be collected to.
-# Don't put anything in this directory yourself; store your static files
-# in apps' "static/" subdirectories and in STATICFILES_DIRS.
-# Example: "/var/www/example.com/static/"
 STATIC_ROOT = 'staticfiles'
-
-# URL prefix for static files.
-# Example: "http://example.com/static/", "http://static.example.com/"
 STATIC_URL = '/static/'
 
-# Additional locations of static files
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
-# List of finder classes that know how to find static files in
-# various locations.
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-#    'django.contrib.staticfiles.finders.DefaultStorageFinder',
+    # 'django.contrib.staticfiles.finders.DefaultStorageFinder',
 )
 
-# Make this unique, and don't share it with anybody.
 SECRET_KEY = os.environ['SECRET_KEY']
-
-# List of callables that know how to import templates from various sources.
 
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-            # Always use forward slashes, even on Windows.
-            # Don't forget to use absolute paths, not relative paths.
             os.path.join(BASE_DIR, 'templates'),
         ],
         'APP_DIRS': True,
@@ -126,13 +74,11 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    # Uncomment the next line for simple clickjacking protection:
-    # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
 ROOT_URLCONF = 'escenario.urls'
 
-# Python dotted path to the WSGI application used by Django's runserver.
 WSGI_APPLICATION = 'escenario.wsgi.application'
 
 INSTALLED_APPS = (
@@ -142,10 +88,7 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # Uncomment the next line to enable the admin:
     'django.contrib.admin',
-    # Uncomment the next line to enable admin documentation:
-    # 'django.contrib.admindocs',
     'generator',
     'django_summernote',
 )
@@ -181,11 +124,6 @@ ESCENARIO_CACHE = {
 ADMIN_MEDIA_PREFIX = '/static/admin/'
 SESSION_SERIALIZER = 'django.contrib.sessions.serializers.JSONSerializer'
 
-# A sample logging configuration. The only tangible logging
-# performed by this configuration is to send an email to
-# the site admins on every HTTP 500 error when DEBUG=False.
-# See http://docs.djangoproject.com/en/dev/topics/logging for
-# more details on how to customize your logging configuration.
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
@@ -210,13 +148,19 @@ LOGGING = {
     }
 }
 
-if os.environ.get('ENVIRONMENT') == 'PROD':
+if os.environ.get('ESCENARIO_ENVIRONMENT') == 'PROD':
     import dj_database_url
     db_url = os.environ['HEROKU_POSTGRESQL_YELLOW_URL']
-    DATABASES['default'] = dj_database_url.config(default=db_url)
+    DATABASES = {
+        'default': dj_database_url.config(default=db_url)
+    }
 else:
-    DATABASES['default'] = {'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'dev.db'}
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': 'dev.db'
+        }
+    }
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
